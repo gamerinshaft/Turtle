e94108a084d6254849f0e18637c76ad08393b0fc
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-05-16 14:16:32 +0900

    indentをそろえた

--- Turtle.java
+++ Turtle.java
@@ -1,47 +1,42 @@
-import java.awt.*; 
+import java.awt.*;
 import java.util.*;
 public class Turtle {
-    //　インスタンス変数の宣言
-    double x, y;
-    double angle;
-    ArrayList<Point> points = new ArrayList<Point>();
- 
-    //この他にも必要な変数を宣言する。ArrayList用の変数など
+  //　インスタンス変数の宣言
+  double x, y;
+  double angle;
+  ArrayList<Point> points = new ArrayList<Point>();
 
-    Turtle(int i, int j) {
-	x = i; y = j; angle =0;
-	points.add(new Point(i,j));
+  //この他にも必要な変数を宣言する。ArrayList用の変数など
+  Turtle(int i, int j) {
+    x = i; y = j; angle =0;
+    points.add(new Point(i,j));
+  }
+
+  //各種メソッドの定義
+  public void move(double length) {
+    double length_x = Math.cos(angle*Math.PI/180)*length;
+    double length_y = Math.sin(angle*Math.PI/180)*length;
+    x = x + length_x;
+    y = y - length_y;
+    points.add(new Point( (int)Math.round(x),(int)Math.round(y)));
+  }
+
+  public void turn(double deg) {
+    angle = angle + deg;
+    //向きを変える処理
+  }
+  public void penSize(){
+  }
+  public void penColor(){
+  }
+
+  public void paint(Graphics g) {
+    int i;
+    Point pt1,pt2;
+    for(i=0;i<points.size()-1;i++){
+      pt1 = points.get(i);
+      pt2 = points.get(i+1);
+      g.drawLine(pt1.x, pt1.y, pt2.x, pt2.y);
     }
-    
-    //各種メソッドの定義
-    public void move(double length) {
-        double length_x = Math.cos(angle*Math.PI/180)*length;
-	double length_y = Math.sin(angle*Math.PI/180)*length;
-	x = x + length_x;
-	y = y - length_y;
-	points.add(new Point( (int)Math.round(x),(int)Math.round(y)));
-    }
-
-    public void turn(double deg) {
-	angle = angle + deg;
-	//向きを変える処理
-    }
-
-    public void penSize(){
-
-    }
-
-    public void penColor(){
-
-    }
-
-    public void paint(Graphics g) {
-	int i;
-	Point pt1,pt2;
-	for(i=0;i<points.size()-1;i++){
-	    pt1 = points.get(i);
-	    pt2 = points.get(i+1);
-	    g.drawLine(pt1.x, pt1.y, pt2.x, pt2.y);
-	}
-    }
+  }
 }
--- TurtleApplet.java
+++ TurtleApplet.java
@@ -3,46 +3,45 @@ import java.awt.*;
 import java.applet.Applet;
 
 public class TurtleApplet extends Applet {
-   Turtle kamekichi;	// 「亀吉」用インスタンス変数の宣言
-
-   public void init() {
-      setBackground(Color.white); // アプレットの背景色を白に設定
-      kamekichi = new Turtle(200,100);	// 「亀吉」の生成
-      // triangles(kamekichi, 40, 2, 10); 三角形（その１）
-      // triangles(kamekichi, 40, 2, 20);  三角形（その2）
-      // square(kamekichi);	// 「亀吉」を正方形に動かす
-      
-   }
-
-   void square(Turtle t) {
-      for (int i = 0; i < 4; i++) {
-	 t.move(50);
-         t.turn(90);
-      }
-   }
-    // 三角形の回転を行うメソッド
+  Turtle kamekichi; // 「亀吉」用インスタンス変数の宣言
+  public void init() {
+    setBackground(Color.white); // アプレットの背景色を白に設定
+    kamekichi = new Turtle(200,100);	// 「亀吉」の生成
     // triangles(kamekichi, 40, 2, 10); 三角形（その１）
-    // triangles(kamekichi, 40, 2, 20); 三角形（その２）
+    // triangles(kamekichi, 40, 2, 20);  三角形（その2）
+    // square(kamekichi);	// 「亀吉」を正方形に動かす
 
-    void triangles(Turtle t, int n, int c, int d) {
-	for (int i = 0; i++ < n; ) {
-	    triangle(t, i * c);
-	    t.turn(d);
-	}
+  }
+
+  void square(Turtle t) {
+    for (int i = 0; i < 4; i++) {
+      t.move(50);
+      t.turn(90);
     }
+  }
+  // 三角形の回転を行うメソッド
+  // triangles(kamekichi, 40, 2, 10); 三角形（その１）
+  // triangles(kamekichi, 40, 2, 20); 三角形（その２）
 
-    void triangle(Turtle t, int size) {
-	t.move(size);
-	t.turn(120);
-	t.move(size);
-	t.turn(120);
-	t.move(size);
-	t.turn(120);
+  void triangles(Turtle t, int n, int c, int d) {
+    for (int i = 0; i++ < n; ) {
+      triangle(t, i * c);
+      t.turn(d);
     }
+  }
 
+  void triangle(Turtle t, int size) {
+    t.move(size);
+    t.turn(120);
+    t.move(size);
+    t.turn(120);
+    t.move(size);
+    t.turn(120);
+  }
 
-   public void paint(Graphics g) {
-      kamekichi.paint(g); // 「亀吉」に足跡を描かせる
-   }
+  public void paint(Graphics g) {
+    kamekichi.paint(g); // 「亀吉」に足跡を描かせる
+  }
+
 }
 
